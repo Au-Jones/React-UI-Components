c2a4773d2e199412b64ab0831ced62665b9f250a
Your Name <aujones95@gmail.com>
2019-04-29 15:32:02 -0400

    finished the headercontent git clear

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+import '.Header.css';
+
+function HeaderTitle (){
+    return (
+        <div class Name='HeaderTitle'>
+        <div className='name'>Lambda School</div>
+        <div className='handle'>@LambdaSchool</div>
+        <div className='date'>&#183; {moment ().format('Do MMM')}</div>
+        </div>
+    )
+}
+
+export default HeaderTitle
