49d2a3437a248dd3a0354c320b85582a67d27b79
Your Name <aujones95@gmail.com>
2019-04-30 15:09:19 -0400

    finally finished the app.js

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -16,6 +16,28 @@ const App = () => {
           inside of this file in order to make them work.
         </strong>
       </p>
+      <NumberButton customClass="black-btn" number="0"  />
+	<br />
+	<NumberButton customClass="blank-btn" number="clear"  />
+	<ActionButton customClass="action-btn" action="÷" />  
+	<br />  
+	<NumberButton customClass="number-btn" number="7" />
+        <NumberButton customClass="number-btn" number="8" />
+        <NumberButton customClass="number-btn" number="9" />
+	<ActionButton customClass="action-btn" action="×" />
+	<br /> 
+	<NumberButton customClass="number-btn" number="4" />
+        <NumberButton customClass="number-btn" number="5" />
+        <NumberButton customClass="number-btn" number="6" /> 
+	<ActionButton customClass="action-btn" action="−" />
+	<br />  
+	<NumberButton customClass="number-btn" number="1" />
+	<NumberButton customClass="number-btn" number="2" />
+	<NumberButton customClass="number-btn" number="3" /> 
+	<ActionButton customClass="action-btn" action="+" />
+	<br />
+	<NumberButton customClass="blank-btn" number="0"  />
+        <ActionButton customClass="action-btn" action="=" />
     </div>
   );
 };
